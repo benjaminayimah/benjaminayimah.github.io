ac8473bee1af9aee3a607f8ca05036f5a5fbf83e
Benjamin Ayimah <benjaminayimah@gmail.com>
2023-02-20 01:44:52 +0000

    Add files via upload

---
+++ precache-manifest.01bf4e8052b9bb1d51cc1742aaa56b48.js
@@ -0,0 +1,378 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e770db86c67346f19a2d",
+    "url": "/css/app.15efaa92.css"
+  },
+  {
+    "revision": "b6bb93d5c8a60a62c827f31d91b6cea9",
+    "url": "/favemeicom.png"
+  },
+  {
+    "revision": "ff3a6885404af0bbb6b7a04d325a8765",
+    "url": "/img/before_after.ff3a6885.png"
+  },
+  {
+    "revision": "30f44843d11cd6ba3613e467843c6803",
+    "url": "/img/before_after_flexsale_1.30f44843.png"
+  },
+  {
+    "revision": "fca8d411ec6c4ab044cfefb6f14cc604",
+    "url": "/img/before_after_flexsale_2.fca8d411.png"
+  },
+  {
+    "revision": "320aeaf6f21cdf3752d3bf257405c01b",
+    "url": "/img/behance_shots.320aeaf6.jpg"
+  },
+  {
+    "revision": "8945ddeb6136c752bac0390117d0731f",
+    "url": "/img/ben.8945ddeb.png"
+  },
+  {
+    "revision": "40959d043d2b9a10ca1cc132f7c5b7ce",
+    "url": "/img/competitive_audit.40959d04.png"
+  },
+  {
+    "revision": "98505c3ec0b9b5f3d2ca4266690992df",
+    "url": "/img/digital_wireframe.98505c3e.png"
+  },
+  {
+    "revision": "4cc4d47e429c3dd5e1070a70521e8508",
+    "url": "/img/digital_wireframe_flexsale.4cc4d47e.png"
+  },
+  {
+    "revision": "b6bb93d5c8a60a62c827f31d91b6cea9",
+    "url": "/img/favemeicom.b6bb93d5.png"
+  },
+  {
+    "revision": "e15ec77d047823285adad932c74cc09c",
+    "url": "/img/flexsale-refined-1.e15ec77d.png"
+  },
+  {
+    "revision": "4c19c280d3d37a21fcb6d174623b3658",
+    "url": "/img/flexsale-refined-2.4c19c280.png"
+  },
+  {
+    "revision": "f55dd6d1a2e4ca0ff292801694c2bc84",
+    "url": "/img/flexsale-refined-3.f55dd6d1.png"
+  },
+  {
+    "revision": "923c3ad9a7dadc04213ca0f89797f818",
+    "url": "/img/flexsale-refined-4.923c3ad9.png"
+  },
+  {
+    "revision": "a3342eae2d6d45beeab99dac8c7cea0c",
+    "url": "/img/flexsale-refined-5.a3342eae.png"
+  },
+  {
+    "revision": "a40411e100ea0770291f91953a1334bb",
+    "url": "/img/flexsale-refined-6.a40411e1.png"
+  },
+  {
+    "revision": "a27c90083c3316fea7dc7f51abcf1068",
+    "url": "/img/flexsale-refined-7.a27c9008.png"
+  },
+  {
+    "revision": "01de18e22829144fd8ce0d3f7fea63af",
+    "url": "/img/flexsale-refined-8.01de18e2.png"
+  },
+  {
+    "revision": "e166a4c0abb8ff060b140a478ffa37e7",
+    "url": "/img/flexsale_banner.e166a4c0.png"
+  },
+  {
+    "revision": "1b2e0a6b588186f420de7edec06857db",
+    "url": "/img/flexsale_digi_wireframe.1b2e0a6b.png"
+  },
+  {
+    "revision": "d35595951791941e5f992f04d075337a",
+    "url": "/img/flexsale_featured.d3559595.png"
+  },
+  {
+    "revision": "ff10356180a41f2eb6aab06fc02290cc",
+    "url": "/img/flexsale_hifi_1.ff103561.png"
+  },
+  {
+    "revision": "1bbd26e7b3a9faa581def0dbf848096d",
+    "url": "/img/flexsale_thumbnail.1bbd26e7.png"
+  },
+  {
+    "revision": "855f40c738589aeb96d34e7ce38abe31",
+    "url": "/img/hifi_flexsale.855f40c7.png"
+  },
+  {
+    "revision": "84f715840edf1661cfa1521b35f40571",
+    "url": "/img/ideation.84f71584.png"
+  },
+  {
+    "revision": "b5cfb05d580572a05a5467be2c34535d",
+    "url": "/img/instagram-shots.b5cfb05d.jpg"
+  },
+  {
+    "revision": "9f9a9e36e4e795d63b7e48ffd9422a4c",
+    "url": "/img/lo-fi.9f9a9e36.png"
+  },
+  {
+    "revision": "9dc824f7bafd048a0837979c758b8c40",
+    "url": "/img/lofi_prototype.9dc824f7.png"
+  },
+  {
+    "revision": "06afcfa141f73c807efdd2c2da62a4d6",
+    "url": "/img/lofi_prototype_flexsale.06afcfa1.png"
+  },
+  {
+    "revision": "dd03fdb178ffb66917172bd170bbee6f",
+    "url": "/img/mergdata_banner.dd03fdb1.png"
+  },
+  {
+    "revision": "694c01fc2680318ba72d0a8dcaf24cb7",
+    "url": "/img/mergdata_before_after_1.694c01fc.png"
+  },
+  {
+    "revision": "44ec37c931fd20e73b381427f07c8ab0",
+    "url": "/img/mergdata_before_after_2.44ec37c9.png"
+  },
+  {
+    "revision": "8a8962f5d9429757a0add4299aee23c8",
+    "url": "/img/mergdata_digi_variation_1.8a8962f5.png"
+  },
+  {
+    "revision": "77d1ea52277de85aaaba0370aaab7e19",
+    "url": "/img/mergdata_digi_variation_2.77d1ea52.png"
+  },
+  {
+    "revision": "0bc953a064a8aa501efbd20ab662656c",
+    "url": "/img/mergdata_digi_variation_3.0bc953a0.png"
+  },
+  {
+    "revision": "e72cfd0445e9ab23e5c8a1f930f04587",
+    "url": "/img/mergdata_digi_wireframe.e72cfd04.png"
+  },
+  {
+    "revision": "4488b015198e7f385c1080f882b91469",
+    "url": "/img/mergdata_hifi_1.4488b015.png"
+  },
+  {
+    "revision": "32eb31a1e4acef84b6dda875cd0ce27d",
+    "url": "/img/mergdata_hifi_2.32eb31a1.png"
+  },
+  {
+    "revision": "b779b0b349fc28087f9ea6d3b50c0927",
+    "url": "/img/mergdata_hifi_3.b779b0b3.png"
+  },
+  {
+    "revision": "95851f2d7672e06add544d26585f8e46",
+    "url": "/img/mergdata_hifi_4.95851f2d.png"
+  },
+  {
+    "revision": "47a287f00f9e90646bd9eea16dd1043a",
+    "url": "/img/mergdata_hifi_5.47a287f0.png"
+  },
+  {
+    "revision": "d925635f8acf6f8ba5da3b56cc8775c6",
+    "url": "/img/mergdata_mob_1.d925635f.png"
+  },
+  {
+    "revision": "ff71fd85c312f401cb19d24878f1fc8d",
+    "url": "/img/mergdata_mob_2.ff71fd85.png"
+  },
+  {
+    "revision": "39c019de30bd9ab3e48f97eccac633fe",
+    "url": "/img/mergdata_mob_3.39c019de.png"
+  },
+  {
+    "revision": "d99de359e21d61207a89cd722f029d36",
+    "url": "/img/mergdata_paper_wireframe.d99de359.png"
+  },
+  {
+    "revision": "bdcf4f26e7c94dae93672708472255a3",
+    "url": "/img/mergdata_refined.bdcf4f26.png"
+  },
+  {
+    "revision": "3d9f50806950f35751486cdcf6d4b12d",
+    "url": "/img/mergdata_thumbnail.3d9f5080.png"
+  },
+  {
+    "revision": "aa9049ee2cb4def6a80556c28d385e63",
+    "url": "/img/oculus_before_after_1.aa9049ee.png"
+  },
+  {
+    "revision": "b02c76d851da227aa1923a8a99fdc725",
+    "url": "/img/oculus_before_after_2.b02c76d8.png"
+  },
+  {
+    "revision": "4256ed8ad23f0e1b9c19f6bde5013dba",
+    "url": "/img/oculus_digi_wireframe.4256ed8a.png"
+  },
+  {
+    "revision": "ef278fde10dd29b7e5b0c52c630a2c5d",
+    "url": "/img/oculus_hifi_1.ef278fde.png"
+  },
+  {
+    "revision": "2906fb7ba16815036bb1db9b1bb08632",
+    "url": "/img/oculus_hifi_2.2906fb7b.png"
+  },
+  {
+    "revision": "5803dc4b7c5783a2a673cecc752b67a9",
+    "url": "/img/oculus_hifi_3.5803dc4b.png"
+  },
+  {
+    "revision": "9cc7f62b8caaf6b57fde381ce4a582c4",
+    "url": "/img/oculus_hifi_4.9cc7f62b.png"
+  },
+  {
+    "revision": "fb8361fdbb0334101471822302a00c95",
+    "url": "/img/oculus_hifi_5.fb8361fd.png"
+  },
+  {
+    "revision": "eafd8a629c87969477274a09f6b59009",
+    "url": "/img/oculus_hifi_6.eafd8a62.png"
+  },
+  {
+    "revision": "08fcb474eeed3206b20776c8b5d8f02d",
+    "url": "/img/oculus_hifi_7.08fcb474.png"
+  },
+  {
+    "revision": "12a4857d39bcec50508a57891ce88bc0",
+    "url": "/img/oculus_hifi_8.12a4857d.png"
+  },
+  {
+    "revision": "be5c9791d68989ac40cdceb964c619c3",
+    "url": "/img/oculus_hifi_9.be5c9791.png"
+  },
+  {
+    "revision": "2dcff612f52c34e04fe1aee3d2315f0d",
+    "url": "/img/paper_wireframe_1.2dcff612.png"
+  },
+  {
+    "revision": "9f53fe62aadc1de52d01dd0e44599839",
+    "url": "/img/paper_wireframe_flexsale.9f53fe62.png"
+  },
+  {
+    "revision": "d7553620a763c9e12524259ad7c88d32",
+    "url": "/img/paper_wireframes.d7553620.png"
+  },
+  {
+    "revision": "4b2330bb1d2ffd11c8332bd78f52e685",
+    "url": "/img/person-2.4b2330bb.png"
+  },
+  {
+    "revision": "112b3a85f42af468bb1bd0ddb007fe3f",
+    "url": "/img/persona_flexsale.112b3a85.png"
+  },
+  {
+    "revision": "052f8805c7e29dc68cdae20f72a11756",
+    "url": "/img/persona_oculus.052f8805.png"
+  },
+  {
+    "revision": "76ef1d1ce452bd969c8b31d396ded0f9",
+    "url": "/img/persona_shortcode.76ef1d1c.png"
+  },
+  {
+    "revision": "7211ac60a87f07f79f2720a549baad77",
+    "url": "/img/prod_delight.7211ac60.svg"
+  },
+  {
+    "revision": "f93f8a4ec13156107b1479d15d41b5f5",
+    "url": "/img/project-2-banner.f93f8a4e.png"
+  },
+  {
+    "revision": "b36853f070c71e9073fde21a2b9d1092",
+    "url": "/img/project-2-thumbnail.b36853f0.png"
+  },
+  {
+    "revision": "7cf823eaeb3b67c81521847bb6429fe6",
+    "url": "/img/project-3-banner.7cf823ea.png"
+  },
+  {
+    "revision": "fc317a17d58922688b4623bdb8309a72",
+    "url": "/img/project-3-thumbnail.fc317a17.png"
+  },
+  {
+    "revision": "01edce1c4f17017a978c01b8d81d2df9",
+    "url": "/img/project-4-banner.01edce1c.png"
+  },
+  {
+    "revision": "88f33b80deaf95b2e3ba505586c069ee",
+    "url": "/img/project-4-thumbnail.88f33b80.png"
+  },
+  {
+    "revision": "784938c567a9114d0189f4f964beb72a",
+    "url": "/img/sample.784938c5.png"
+  },
+  {
+    "revision": "8618a0606a1de53fa2a12c09a1a1f16b",
+    "url": "/img/screen_variations.8618a060.png"
+  },
+  {
+    "revision": "86b32bbee229788d268f4755fde8dd0f",
+    "url": "/img/shortcode_banner.86b32bbe.png"
+  },
+  {
+    "revision": "e536d0dbb62c618d6a92668dd09d13d1",
+    "url": "/img/shortcode_before_after_1.e536d0db.png"
+  },
+  {
+    "revision": "1eb6b20200742a1a7c4f227f15ac7604",
+    "url": "/img/shortcode_hifi_1.1eb6b202.png"
+  },
+  {
+    "revision": "4aaaf353b3e1804c44288020634307ee",
+    "url": "/img/shortcode_hifi_2.4aaaf353.png"
+  },
+  {
+    "revision": "537d0ffcea3e1c497fe23e3003ee3aa2",
+    "url": "/img/shortcode_hifi_3.537d0ffc.png"
+  },
+  {
+    "revision": "f8419f90170cce773d24dac31318a531",
+    "url": "/img/shortcode_hifi_4.f8419f90.png"
+  },
+  {
+    "revision": "41d58dae956967817d7bb7dccf7ed25e",
+    "url": "/img/shortcode_hifi_5.41d58dae.png"
+  },
+  {
+    "revision": "f9da3ac510297baa297eb1b3fc717d19",
+    "url": "/img/shortcode_paper_wireframe.f9da3ac5.png"
+  },
+  {
+    "revision": "3b2f53b979f6540b0c4c525a7588c7af",
+    "url": "/img/shortcode_thumbnail.3b2f53b9.png"
+  },
+  {
+    "revision": "98cbe231bcf6fd08b05437ac05b0d2fe",
+    "url": "/img/sitemap.98cbe231.png"
+  },
+  {
+    "revision": "715dc808df8d3174dc1ff77d95db90e9",
+    "url": "/img/ui.715dc808.svg"
+  },
+  {
+    "revision": "4628fc8826eaf0739663cdce2ed1d0e1",
+    "url": "/img/user_journey_map.4628fc88.png"
+  },
+  {
+    "revision": "21465150db3e85d33ca6cf36cdabc728",
+    "url": "/img/ux.21465150.svg"
+  },
+  {
+    "revision": "4c935a211a2247fcde91d78f7ad328e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e770db86c67346f19a2d",
+    "url": "/js/app.35fd6b67.js"
+  },
+  {
+    "revision": "e4bbe2a475492a331e12",
+    "url": "/js/chunk-vendors.7d684dec.js"
+  },
+  {
+    "revision": "0328cb5657a846b1043cd038e6311e82",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
